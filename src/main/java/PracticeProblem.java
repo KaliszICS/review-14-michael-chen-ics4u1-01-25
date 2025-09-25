2270f87dcb4f791dec173868f3eddfc9f467a258
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-09-25 13:58:38 -0400

    test

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,33 @@ public class PracticeProblem {
 
 	}
 
+	public static int calculate(int a, int b, char c) {
+		switch (c) {
+			case ('+'): return a + b;
+			case ('-'): return a - b;
+			case ('*'): return a * b;
+			case ('/'): return a / b;
+			case ('%'): return a % b;
+			case ('^'): return (int)Math.pow(a, b);
+			default: return -1;
+		}
+	}
+
+	public static boolean totalWordsChecker(String a) {
+		return a.split(" ").length <= 10 ? true : false;
+	}
+
+	public static String minString(String a, String b, String c) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+		c = c.toLowerCase();
+		if (a.compareTo(b) < 0 && a.compareTo(c) < 0) {
+			return a;
+		} else if (b.compareTo(a) < 0 && b.compareTo(c) < 0) {
+			return b;
+		} else {
+			return c;
+		}
+
+	}
 }
